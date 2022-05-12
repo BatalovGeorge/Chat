059875e7dcd0441a95d311f3618cb73c84ae8d08
Gosha <batalov.george@gmail.com>
2022-05-13 02:17:46 +0300

    add models, contact cads

--- ModernChat/MVVM/ViewModel/MainViewModel.cs
+++ ModernChat/MVVM/ViewModel/MainViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ModernChat.MVVM.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,70 @@ namespace ModernChat.MVVM.ViewModel
 {
     internal class MainViewModel
     {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
 
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Саня",
+                UsernameColor = "#409aff",
+                ImageSource = "https://i.imgur.com/ddkrJYj.png",
+                Message = "Test",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FirstMessage = true
+            });
+
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Саня",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/ddkrJYj.png",
+                    Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = false,
+                    FirstMessage = false
+                });
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Антон",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/ddkrJYj.png",
+                    Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = true
+                });
+            }
+
+                Messages.Add(new MessageModel
+                {
+                    Username = "Санек",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/ddkrJYj.png",
+                    Message = "Last",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = true
+                });
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"Sanya{i}",
+                    ImageSource= "https://i.imgur.com/GtolBwW.png",
+                    Messages = Messages
+                });
+            }
+        }
     }
 }
